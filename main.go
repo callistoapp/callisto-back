e1929c80dad54f82deb8ebd6cb51581b5e124571
cmondion <clement.mondion@clicrdv.com>
2018-03-24 16:28:29 +0100

    cleaner and safer main.go

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	pb "callisto/authorization"
 	"time"
 	"golang.org/x/net/context"
+	"github.com/labstack/gommon/log"
 )
 
 
@@ -41,33 +42,57 @@ func executeQuery(query QueryStruct, schema graphql.Schema) *graphql.Result {
 	return result
 }
 
-func verifyToken(client pb.AuthorizeClient, token *pb.CallistoToken) {
-	fmt.Printf("Verifying token: (%d)\n", token.Token)
+func verifyToken(client pb.AuthorizeClient, token *pb.CallistoToken) (bool, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	logged, err := client.Authorize(ctx, token)
 	if err != nil {
-		fmt.Printf("%v.Authorize(_) = _, %v: ", client, err)
+		log.Info("%v.Authorize(_) = _, %v: ", client, err)
+		return false, err
 	}
-	fmt.Println("Looks like a success ???")
-	fmt.Println(logged.Logged)
+	return logged.Logged, nil
+}
+
+func isLogged(r *http.Request) (bool, error) {
+	conn, err := grpc.Dial("auth:50051",  grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("fail to dial: %v", err)
+		return false, err
+	}
+	defer conn.Close()
+	client := pb.NewAuthorizeClient(conn)
+	token, err := r.Cookie("connect.sid")
+	if err != nil {
+		log.Error("Error occurred while searching connect.sid token : %v", err)
+		return false, err
+	}
+	return verifyToken(client, &pb.CallistoToken{Token: token.Value})
 }
 
 
 func main() {
 	models.InitDB("user=callisto dbname=callisto password=postgrespassword host=postgres sslmode=disable")
 	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "http://app.callisto.com")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers",
 			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 		fmt.Println("Hello from /graphql")
 
 		if r.Body != nil && r.Method == "POST" {
+			logged, err := isLogged(r)
+
+			if logged == false || err != nil {
+				log.Info("Not authorized !")
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte("401 - Unauthorized!"))
+				return
+			}
 			decoder := json.NewDecoder(r.Body)
 			var q QueryStruct
-			err := decoder.Decode(&q)
+			err = decoder.Decode(&q)
 			if err != nil {
 				fmt.Printf("%s", err)
 			}
@@ -84,6 +109,17 @@ func main() {
 		json.NewEncoder(w).Encode("please use post")
 	})
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "localhost")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		fmt.Println("Hello from /")
+
+		json.NewEncoder(w).Encode("Hello From graphql api")
+	})
+
 	http.HandleFunc("/logged", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
@@ -91,13 +127,7 @@ func main() {
 			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 		fmt.Println("Hello from /logged")
-		conn, err := grpc.Dial("auth:50051",  grpc.WithInsecure())
-		if err != nil {
-			fmt.Printf("fail to dial: %v", err)
-		}
-		defer conn.Close()
-		client := pb.NewAuthorizeClient(conn)
-		verifyToken(client, &pb.CallistoToken{Token: "nifejnovervrenvrereoj"})
+		isLogged(r)
 	})
 	// Display some basic instructions
 	fmt.Println("Now server is running on port 8081")
