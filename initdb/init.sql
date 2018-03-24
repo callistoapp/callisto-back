bb0692aaf1dadb94906136d8c58d0b9896f6f703
cmondion <clement.mondion@clicrdv.com>
2018-03-24 16:27:56 +0100

    upate init script to fix errors

--- initdb/init.sql
+++ initdb/init.sql
@@ -8,6 +8,15 @@ CREATE TABLE projects (
   deleted     INTEGER NOT NULL DEFAULT 0
 );
 
+CREATE TABLE statuses (
+  id          SERIAL PRIMARY KEY,
+  projectId   INTEGER REFERENCES projects (id),
+  name        VARCHAR(140) NOT NULL,
+  description VARCHAR(140),
+  index       INTEGER,
+  deleted     INTEGER NOT NULL DEFAULT 0
+);
+
 CREATE TABLE tasks (
   id          SERIAL PRIMARY KEY,
   projectId   INTEGER REFERENCES projects (id),
@@ -15,15 +24,6 @@ CREATE TABLE tasks (
   description VARCHAR(140),
   type        INTEGER,
   statusId    INTEGER REFERENCES statuses (id),
-  deleted     INTEGER NOT NULL DEFAULT 0
-);
-
-CREATE TABLE statuses (
-  id          SERIAL PRIMARY KEY,
-  projectId   INTEGER REFERENCES projects (id),
-  name        VARCHAR(140) NOT NULL,
-  description VARCHAR(140),
-  index       INTEGER,
   deleted     INTEGER NOT NULL DEFAULT 0
 );
 
