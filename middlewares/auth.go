13e7085df6754e034f0e94798e8cfbf6c339e075
cmondion <clement.mondion@clicrdv.com>
2018-05-17 18:07:10 +0200

    chore(middlewares): split auth logic to middleware

---
+++ middlewares/auth.go
@@ -0,0 +1,37 @@
+package middlewares
+
+import (
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"callisto/models"
+	"github.com/gorilla/context"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("connect.sid")
+		if err != nil || cookie == nil {
+			http.Error(w, "Forbidden", http.StatusForbidden)
+			return
+		}
+
+		client := &http.Client{}
+		if req, err := http.NewRequest("GET", "http://auth:3001/profile", nil); err != nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		} else {
+			req.AddCookie(cookie)
+			resp, err := client.Do(req)
+			if err != nil {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+			body, _ := ioutil.ReadAll(resp.Body)
+			var loggedUser models.AuthenticatedUser
+			json.Unmarshal([]byte(body), &loggedUser)
+			context.Set(r,"loggedUser", loggedUser)
+			next.ServeHTTP(w, r)
+		}
+	})
+}
