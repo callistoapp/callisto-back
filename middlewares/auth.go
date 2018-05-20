64fcabc657bfd40b2be1f70f69cdb69d0c3b0899
cmondion <clement.mondion@clicrdv.com>
2018-05-20 17:34:21 +0200

    fix(cors): dirty fix to avoid errors when 401

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -10,6 +10,12 @@ import (
 
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "https://app.callisto.com")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
 		if r.Method != "POST" || r.RequestURI != "/graphql" {
 			next.ServeHTTP(w, r)
 			return
